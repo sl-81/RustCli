238125f11223f020ed5ce35b8997f6841781ccfd
sl-81 <s_li1992@hotmail.com>
2024-06-03 12:20:50 -0700

    initial commit

---
+++ rget/target/debug/build/libc-708bf80015eee4c0/build_script_build-708bf80015eee4c0.d
@@ -0,0 +1,5 @@
+/Users/shiyuli/Desktop/codes/RustCli/rget/target/debug/build/libc-708bf80015eee4c0/build_script_build-708bf80015eee4c0: /Users/shiyuli/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.155/build.rs
+
+/Users/shiyuli/Desktop/codes/RustCli/rget/target/debug/build/libc-708bf80015eee4c0/build_script_build-708bf80015eee4c0.d: /Users/shiyuli/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.155/build.rs
+
+/Users/shiyuli/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.155/build.rs:
