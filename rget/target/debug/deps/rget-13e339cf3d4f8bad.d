5775e9a4a889302eb3fb626088281742366f7ab4
sl-81 <s_li1992@hotmail.com>
2024-06-03 12:18:19 -0700

    initial commit

---
+++ rget/target/debug/deps/rget-13e339cf3d4f8bad.d
@@ -0,0 +1,5 @@
+/Users/shiyuli/Desktop/codes/RustCli/rget/target/debug/deps/librget-13e339cf3d4f8bad.rmeta: src/main.rs
+
+/Users/shiyuli/Desktop/codes/RustCli/rget/target/debug/deps/rget-13e339cf3d4f8bad.d: src/main.rs
+
+src/main.rs:
---
+++ rget/target/debug/deps/rget-6a958d053999eea2.d
@@ -0,0 +1,5 @@
+/Users/shiyuli/Desktop/codes/RustCli/rget/target/debug/deps/librget-6a958d053999eea2.rmeta: src/main.rs
+
+/Users/shiyuli/Desktop/codes/RustCli/rget/target/debug/deps/rget-6a958d053999eea2.d: src/main.rs
+
+src/main.rs:
